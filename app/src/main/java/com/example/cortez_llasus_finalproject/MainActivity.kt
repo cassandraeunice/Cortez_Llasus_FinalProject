5667fc4fa55df61d8473e91fabeb8fed303eb340
cc112 <cassandraeunice.cortez.cics@ust.edu.ph>
2023-11-10 00:40:32 +0800

    login page pa lang

---
+++ app/src/main/java/com/example/cortez_llasus_finalproject/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cortez_llasus_finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.*
+import com.airbnb.lottie.LottieAnimationView
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val ANIMATION_DURATION: Long = 9000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val welcomeAnimationView: LottieAnimationView = findViewById(R.id.welcomeanimation)
+
+        welcomeAnimationView.playAnimation()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this@MainActivity, LoginPage::class.java)
+            startActivity(intent)
+        }, ANIMATION_DURATION)
+
+    }
+}
